654814d94ef4b11a4b1d327ede0a70b36fc6bddf
Lasse Hjøllund Jensen <lasse.hjollund@Gmail.com>
2025-10-01 13:17:45 +0200

    changes to lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,10 +1,45 @@
 return {
   {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "jdtls" },
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- Keymaps for LSP features
+      local on_attach = function(_, bufnr)
+        local opts = {buffer = bufnr }
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+      end
+
+      -- Setup Lua LSP
+      lspconfig.lua_ls.setup({
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            diagnostics = { globals = { 'vim' } },
+          },
+        },
+      })
+
+      -- Setup
+      lspconfig.jdtls.setup({
+        on_attach = on_attach
+      })
+    end
   }
 }
